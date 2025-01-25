8c10e7209a166dc7c3a04b5b3e3cb5e58c7d66de
mtsnowden <mtsnowden@gmail.com>
2025-01-24 20:52:09 -0600

    working on hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <main className="font-light text-white antialiased selection:bg-lime-300 selection:text-black">
       <Navbar />
+      <Hero />
     </main>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+<section>
+    <div className="flex flex-col items-center justify-center">
+        <h1 className="mt-16 overflow-hidden text-[12vw] font-semibold uppercase leading-none">
+            Benson <br /> Langford
+        </h1>
+    </div>
+</section>  
+)
+}
+
+export default Hero
