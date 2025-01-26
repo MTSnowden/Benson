fdaeb3c39e24fbe2c1376e689bf08f03a54d7332
mtsnowden <mtsnowden@gmail.com>
2025-01-25 19:09:32 -0600

    working on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,17 @@ const Hero = () => {
         <h1 className="mt-16 overflow-hidden text-[12vw] font-semibold uppercase leading-none">
             Benson <br /> Langford
         </h1>
+        <div className="mt-8">
+            <a href="/Benson-Langford.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            download
+            className='flex items-center rounded-xl bg-lime-300 p-2
+            px-3 font-sans font-medium text-black hover:bg-lime-400'>
+                <span>Resume.pdf</span>
+            </a>
+
+        </div>
     </div>
 </section>  
 )
