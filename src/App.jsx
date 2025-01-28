b182b5bde04c6ccc396211c2fa442fba6cf06164
mtsnowden <mtsnowden@gmail.com>
2025-01-28 15:46:51 -0600

    added marquee section and projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Hero from './components/Hero'
+import Marquee from './components/Marquee'
 import Navbar from './components/Navbar'
+import Projects from './components/Projects'
 
 const App = () => {
   return (
     <main className="font-light text-white antialiased selection:bg-lime-300 selection:text-black">
       <Navbar />
       <Hero />
+      <Marquee />
+      <Projects />
     </main>
   )
 }
